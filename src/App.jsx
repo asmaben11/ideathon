5fad29b762b76938b37e0673f41c6c57dedda6c9
DhiaMerzougui <dhia.merzougui@etu.univ-batna2.dz>
2025-01-03 04:55:45 +0100

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
-import zelda from './assets/images/zelda.png'
-import H from './pages/homepage'
+import Homepage from './pages/homepage'
 
 function App() {
-  let name = "john"
-  let age = 20
-  const Greeting = props =>{
-    return (
-      <>
-      <H/>
-<<<<<<< HEAD
-=======
-        <div className="bg-red-500 p-4 m-4 rounded-lg font-programme text-4xl"> 
-          hello ,{props.name} {props.age}
-        </div>
-        <img src={zelda} alt="zelda" className="w-64 h-64 object-cover" />
->>>>>>> 936dc7a1c4f632da60c15a827de11b4f10f6dd1c
-      </>
-    )
-  }
-  return(
-    <div className="min-h-screen flex items-center justify-center">
-      <Greeting name={name} age={age}/>
-    </div>
+  return (
+    <Homepage />
   )
 }
 
